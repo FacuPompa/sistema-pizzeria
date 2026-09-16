bd801fefced02de04b14a2c2be875c5dcc6e3ef1
FacuPompa <pompafacundo4@gmail.com>
2026-09-16 16:35:39 -0300

    dates&lists query methods

--- src/main/java/com/jpa/pizza/persistence/repository/OrderRepository.java
+++ src/main/java/com/jpa/pizza/persistence/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.jpa.pizza.persistence.repository;
 
 import com.jpa.pizza.persistence.entity.OrderEntity;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
+    List<OrderEntity> findAllByDateAfter(LocalDate date);
+    List<OrderEntity> findAllByMethodIn(List<String> methods);
 }
--- src/main/java/com/jpa/pizza/service/PizzaService.java
+++ src/main/java/com/jpa/pizza/service/PizzaService.java
@@ -25,6 +25,7 @@ public class PizzaService {
     }
 
     public List<PizzaEntity> getAvailable() {
+        System.out.println(this.pizzaRepository.countByVeganTrue());
         return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
     }
 
