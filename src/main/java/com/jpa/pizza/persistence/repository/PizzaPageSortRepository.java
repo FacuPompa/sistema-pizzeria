970cfac29f30721ceeeb4ad73540c4661a8f4b97
FacuPompa <pompafacundo4@gmail.com>
2026-09-17 10:53:39 -0300

    paging and sorting 2

--- src/main/java/com/jpa/pizza/persistence/repository/PizzaPageSortRepository.java
+++ src/main/java/com/jpa/pizza/persistence/repository/PizzaPageSortRepository.java
@@ -1,7 +1,10 @@
 package com.jpa.pizza.persistence.repository;
 
 import com.jpa.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 public interface PizzaPageSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
 }
