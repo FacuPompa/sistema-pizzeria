a2393585e1387e84c15ef147f68863a2d32e9830
FacuPompa <pompafacundo4@gmail.com>
2026-09-23 10:48:37 -0300

    modifying updates @Query

--- src/main/java/com/jpa/pizza/service/PizzaService.java
+++ src/main/java/com/jpa/pizza/service/PizzaService.java
@@ -3,12 +3,14 @@ package com.jpa.pizza.service;
 import com.jpa.pizza.persistence.entity.PizzaEntity;
 import com.jpa.pizza.persistence.repository.PizzaPageSortRepository;
 import com.jpa.pizza.persistence.repository.PizzaRepository;
+import com.jpa.pizza.service.dto.UpdatePizzaPriceDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -64,6 +66,11 @@ public class PizzaService {
 
     public void delete(int idPizza) {
         this.pizzaRepository.deleteById(idPizza);
+    }
+
+    @Transactional
+    public void updatePrice(UpdatePizzaPriceDto dto) {
+        this.pizzaRepository.updatePrice(dto);
     }
 
     public boolean exists(int idPizza) {
